18bec9a73c075bf2c8145ce3c85b576d231527ce
DaSilvaStudio <felipesavory@gmail.com>
2024-06-24 18:53:08 +0100

    Having some problems

--- scr/world.gd
+++ scr/world.gd
@@ -1,8 +1,10 @@
 extends Node3D
 
 var interface : XRInterface
+@onready var diffuseCurve : GradientTexture1D = load("res://new_gradient_texture_1d.tres")
 
 func _ready() -> void:
+	RenderingServer.global_shader_parameter_add("diffuse_curve", RenderingServer.GLOBAL_VAR_TYPE_SAMPLER2D, diffuseCurve)
 	interface = XRServer.find_interface("OpenXR")
 	if interface and interface.initialize():
 		print("!")
